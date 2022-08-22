9de72253c9299b4300d6e9ea34f812bcb0c45428
quocbaole.cs <quocbaole368@gmail.com>
2022-08-22 19:48:12 +0700

    su dung forEach

--- Bai01M2_Lambda/src/thucHanh/Vd2.java
+++ Bai01M2_Lambda/src/thucHanh/Vd2.java
@@ -2,6 +2,8 @@ package thucHanh;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Vd2 {
 	public static void main(String[] args) {
@@ -18,5 +20,21 @@ public class Vd2 {
 
 		System.out.println("Xuat ra man hinh dung forEach");
 		ds.forEach(s -> System.out.println(s));
+
+		Map<String, String> td = new TreeMap<>();
+		td.put("one", "Mot");
+		td.put("two", "Hai");
+		td.put("three", "Ba");
+		td.put("four", "Bon");
+		td.put("five", "Nam");
+		td.put("six", "Sau");
+		td.put("seven", "Bay");
+		System.out.println("Xuat map td ra man hinh dung for");
+		for (String key : td.keySet()) {
+			System.out.println(key + " ->" + td.get(key));
+		}
+		System.out.println("Xuat map td ra man hinh dung forEach");
+		td.forEach((key, value) -> System.out.println(key + "->" + value));
+
 	}
 }
