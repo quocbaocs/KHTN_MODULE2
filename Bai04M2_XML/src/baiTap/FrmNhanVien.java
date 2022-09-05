523ae67b4e093cb188f91842f14fb0f1169b90b7
quocbaole <quocbaole368@gmail.com>
2022-09-05 20:42:20 +0700

    update bai 4.2

--- Bai04M2_XML/src/baiTap/FrmNhanVien.java
+++ Bai04M2_XML/src/baiTap/FrmNhanVien.java
@@ -6,10 +6,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class FrmNhanVien extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable tblNhanVien;
 
 	/**
 	 * Launch the application.
@@ -31,12 +43,39 @@ public class FrmNhanVien extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmNhanVien() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				
+			}
+		});
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 72, 414, 178);
+		contentPane.add(scrollPane);
+		
+		tblNhanVien = new JTable();
+		tblNhanVien.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID", "H\u1ECD t\u00EAn", "Gi\u1EDBi t\u00EDnh", "Ng\u00E0y sinh", "M\u1EE9c l\u01B0\u01A1ng", "\u0110\u1ECBa ch\u1EC9"
+			}
+		));
+		scrollPane.setViewportView(tblNhanVien);
+		
+		JLabel lblNewLabel = new JLabel("Đơn vị");
+		lblNewLabel.setBounds(20, 22, 67, 23);
+		contentPane.add(lblNewLabel);
+		
+		JComboBox cbbDonVi = new JComboBox();
+		cbbDonVi.setBounds(103, 22, 243, 23);
+		contentPane.add(cbbDonVi);
 	}
-
 }
