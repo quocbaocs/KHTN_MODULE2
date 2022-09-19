104d5de4fd2e32ea39cd6a403691ea4d61515d25
quocbaole <quocbaole368@gmail.com>
2022-09-19 20:38:16 +0700

    update sua bai 1

--- Bai07M2_DegisnPattern/src/baiTap/ShapeDecorator.java
+++ Bai07M2_DegisnPattern/src/baiTap/ShapeDecorator.java
@@ -1,26 +1,24 @@
 package baiTap;
 
-public class ShapeDecorator implements Shape{
+public class ShapeDecorator implements Shape {
 	protected Shape shape;
+
 	public ShapeDecorator(Shape shape) {
 		// TODO Auto-generated constructor stub
 		this.shape = shape;
 	}
-	
-	
+
 	public Shape getShape() {
 		return shape;
 	}
-
 
 	public void setShape(Shape shape) {
 		this.shape = shape;
 	}
 
-
 	@Override
 	public String draw() {
 		// TODO Auto-generated method stub
-		return null;
+		return shape.draw();
 	}
 }
